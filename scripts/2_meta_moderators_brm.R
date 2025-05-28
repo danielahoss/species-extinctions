ebdd90b0429a3dbe54c510b29fa097528041e39d
Daniela Hoss <93685263+danielahoss@users.noreply.github.com>
2025-05-28 15:03:37 +0200

    2_meta_moderators_brm.R

--- scripts/2_meta_moderators_brm.R
+++ scripts/2_meta_moderators_brm.R
@@ -1,13 +1,13 @@
 # Meta-Analysis Moderators 
 # model and model checks
 
-
 # 1. Load Libraries and Set Up Environment --------------------------------
 
 library(tidyverse)
 library(brms) 
 library(tidybayes)
 library(ggdist)
+library(DHARMa) 
 
 # Clear workspace 
 rm(list = ls())
@@ -16,139 +16,195 @@ rm(list = ls())
 
 effects_brm <- read.csv(here::here("data", "effects_brm.csv"), header = T) 
 
-# standardizing variables
-moderators_df_pad <- effects_brm %>%
+effects <- effects_brm %>% 
+  ungroup() %>% 
+  group_by(study_ID) %>% 
+  mutate(ES_ij = 1:n()) %>% 
+  ungroup() %>%
   mutate(
     lat_pad = round(as.numeric(scale(as.numeric(latitude_abs), center = TRUE, scale = FALSE)),2),
-    lon_pad = round(as.numeric(scale(as.numeric(longitude), center = TRUE, scale = FALSE)),2),
     ppt_pad = round(as.numeric(scale(as.numeric(mean_annual_ppt_mm), center = TRUE, scale = FALSE)),2),
     temp_pad = round(as.numeric(scale(as.numeric(mean_annual_temperature_Celsius), center = TRUE, scale = FALSE)),2),
     alt_pad = round(as.numeric(scale(as.numeric(altitude_m), center = TRUE, scale = FALSE)),0),
     time_pad = as.numeric(scale(as.numeric(experiment_duration), center = TRUE, scale = FALSE)))
- 
-
-df_pad <- effects_brm %>%
-  select(study_ID,                        
-         experiment_duration,            
-         country,                        
-         latitude,                        
-         latitude_abs,                   
-         longitude,                       
-         altitude_m,                     
-         mean_annual_ppt_mm,              
-         mean_annual_temperature_Celsius,
-         author,                          
-         publ_year,                      
-         eff_size_ctrl,                   
-         eff_size_rem,                   
-         sd_eff_size_ctrl,                
-         sd_eff_size_rem,                
-         n_control,                       
-         n_removal,                      
-         se_eff_size_ctrl,                
-         se_eff_size_rem)  
 
 
+# Supplementary Fig. 2 - mean annual precipitation ------------------------
 
-
-# 3. Model_mean annual ppt ------------------------------------------------
-
-
-# mod_ppt <-
-#   brm(data = moderators_df_pad,
-#       yi | se(sei) ~ 1 + ppt_pad + (1 | study_ID),
-#       iter = 4000, warmup = 1000,
-#       control = list(max_treedepth = 14),
-#       cores = 4, chains = 4, file = "ma_moderators_ppt")
+# set.seed(123)
+# brm(data = effects,
+#     yi | se(sei) ~ 1 + ppt_pad + (1 | study_ID/ES_ij),
+#     iter = 4000, warmup = 1000,
+#     control = list(max_treedepth = 14),
+#     cores = 6, chains = 4, file = "model_output/ma_moderators_ppt")
 mod_ppt <- read_rds(here::here("model_output", "ma_moderators_ppt.rds")) 
 
 mod_ppt %>% summary
+
 pp_check(mod_ppt) 
+conditional_effects(mod_ppt)
 
 
+model.check <- createDHARMa(
+  simulatedResponse = t(posterior_predict(mod_ppt)),
+  observedResponse = effects$yi,
+  fittedPredictedResponse = apply(t(posterior_epred(mod_ppt)), 1, mean),
+  integerResponse = TRUE)
 
-# 4. Model_mean annual temperature ---------------------------------
 
+plot(model.check)  # qq and residuals
+
+testDispersion(model.check)
+
+
+# Supplementary Fig. 3 - mean annual temperature --------------------------
+
+
+effects_temp <- effects
+effects_temp$study_ID [(effects_temp$study_ID == "ID_239" & effects_temp$temp_pad == 0.85 )] <- "ID_239_cold"
+
+# set.seed(123)
 # mod_temp <-
-#   brm(data = moderators_df_pad,
-#       yi | se(sei) ~ 1 + temp_pad + (1 | study_ID),
+#   brm(data = effects_temp,
+#       yi | se(sei) ~ 1 + temp_pad + (1 | study_ID/ES_ij),
 #       iter = 4000, warmup = 1000,
-#       cores = 4, chains = 4 , file = "ma_moderators_temp")
+#       cores = 6, chains = 4, file = "model_output/ma_moderators_temp")
 mod_temp <- read_rds(here::here("model_output", "ma_moderators_temp.rds")) 
 
 mod_temp %>% summary
 
 mod_temp %>% pp_check
 
+mod_temp %>% conditional_effects()
 
 
-# 5. Model_time_pad ------------------------------------------------
+moders_temp <- effects %>% 
+  filter(temp_pad != "NA" ) 
 
-# mod_time <-
-#   brm(data = moderators_df_pad,
-#       yi | se(sei) ~ 1 + time_pad + (1 | study_ID),
-#       iter = 4000, warmup = 1000,
-#       cores = 4, chains = 4, file = "ma_moderators_time")
-mod_time <- read_rds(here::here("model_output", "ma_moderators_time.rds")) 
-
-mod_time %>% summary
+model.check <- createDHARMa(
+  simulatedResponse = t(posterior_predict(mod_temp)),
+  observedResponse = moders_temp$yi,
+  fittedPredictedResponse = apply(t(posterior_epred(mod_temp)), 1, mean),
+  integerResponse = TRUE)
 
 
-pp_check(mod_time) 
+plot(model.check)  # qq and residuals
+
+testDispersion(model.check)
+
+# Supplementary Fig. 4 - abs(latitude) ------------------------------------
 
 
-
-# 6. Model_latitude ------------------------------------------------
-
-
+# set.seed(123)
 # mod_lat <-
-#   brm(data = moderators_df_pad,
-#       yi | se(sei) ~ 1 + lat_pad + (1 | study_ID),
+#   brm(data = effects,
+#       yi | se(sei) ~ 1 + latitude_abs + (1 | study_ID/ES_ij),
 #       iter = 4000, warmup = 1000,
-#       cores = 4, chains = 4 , file = "ma_moderators_lat")
+#       cores = 6, chains = 4, file = "model_output/ma_moderators_lat")
 
 mod_lat <- read_rds(here::here("model_output", "ma_moderators_lat.rds")) 
-mod_lat %>% summary
 
+mod_lat %>% summary
 
 pp_check(mod_lat) 
 conditional_effects(mod_lat)
 
+model.check <- createDHARMa(
+  simulatedResponse = t(posterior_predict(mod_lat)),
+  observedResponse = effects$yi,
+  fittedPredictedResponse = apply(t(posterior_epred(mod_lat)), 1, mean),
+  integerResponse = TRUE)
 
 
+plot(model.check)  # qq and residuals
 
-# 7. Model_response_variable ---------------------------------------
+testDispersion(model.check)
 
+
+# Supplementary Fig. 5 - response_variable --------------------------------
+
+
+# set.seed(123)
 # mod_resp <-
-#   brm(data = moderators_df_pad,
-#       yi | se(sei) ~ 0 + response_variable + (1 | study_ID),
+#   brm(data = effects,
+#       yi | se(sei) ~ 0 + response_variable + (1 | study_ID/ES_ij),
 #       iter = 4000, warmup = 1000,
-#       cores = 4, chains = 4 , file = "ma_moderators_response_variable")
+#       cores = 6, chains = 4, file = "model_output/ma_moderators_response_variable")
 mod_resp <- read_rds(here::here("model_output", "ma_moderators_response_variable.rds")) 
 
 mod_resp %>% summary
-
-conditional_effects(mod_resp)
 pp_check(mod_resp) 
+conditional_effects(mod_resp)
+
+model.check <- createDHARMa(
+  simulatedResponse = t(posterior_predict(mod_resp)),
+  observedResponse = effects$yi,
+  fittedPredictedResponse = apply(t(posterior_epred(mod_resp)), 1, mean),
+  integerResponse = TRUE)
 
 
+plot(model.check)  # qq and residuals
 
-# 8. Model_effect_type ---------------------------------------------
+testDispersion(model.check)
 
 
+# Supplementary Fig. 6 - Experiment duration ------------------------------
 
-# mod_effect_type <-
-#   brm(data = moderators_df_pad,
-#       yi | se(sei) ~ 0 + effect_type + (1 | study_ID),
+# set.seed(123)
+# mod_time <-
+#   brm(data = effects,
+#       yi | se(sei) ~ 1 + time_pad + (1 | study_ID/ES_ij),
 #       iter = 4000, warmup = 1000,
-#       cores = 4, chains = 4 , file = "ma_moderators_effect_type")
-mod_effect_type <- read_rds(here::here("model_output", "ma_moderators_effect_type.rds")) 
+#       cores = 6, chains = 4, file = "model_output/ma_moderators_time")
+mod_time <- read_rds(here::here("model_output", "ma_moderators_time.rds")) 
 
-mod_effect_type %>% summary
+mod_time %>% summary
+pp_check(mod_time) 
+mod_time %>% conditional_effects()
 
-conditional_effects(mod_effect_type)
+model.check <- createDHARMa(
+  simulatedResponse = t(posterior_predict(mod_time)),
+  observedResponse = effects$yi,
+  fittedPredictedResponse = apply(t(posterior_epred(mod_time)), 1, mean),
+  integerResponse = TRUE)
 
-pp_check(mod_effect_type) 
 
+plot(model.check)  # qq and residuals
+
+testDispersion(model.check)
+
+
+
+# multimoderator ----------------------------------------------------------
+
+# set.seed(123)
+# mod_covars <-
+#   brm(data = effects,
+#       yi | se(sei) ~ 1 + lat_pad + ppt_pad +
+#         temp_pad + (1 | study_ID/ES_ij),
+#       iter = 4000, warmup = 1000,
+#      # control = list(max_treedepth = 14),
+#       cores = 6, chains = 4, file = "model_output/ma_moderators_lat_ppt_temp_PAD")
+mod_covars <- read_rds(here::here("model_output", "ma_moderators_lat_ppt_temp_PAD.rds")) 
+mod_covars %>% summary
+mod_covars %>% pp_check()
+
+mod_covars %>% conditional_effects()
+
+
+effects_covars <- effects %>% 
+  filter(temp_pad != "NA" ) 
+
+
+model.check <- createDHARMa(
+  simulatedResponse = t(posterior_predict(mod_covars)),
+  observedResponse = effects_covars$yi,
+  fittedPredictedResponse = apply(t(posterior_epred(mod_covars)), 1, mean),
+  integerResponse = TRUE)
+
+
+plot(model.check)  # qq and residuals
+
+testDispersion(model.check)
 
 
